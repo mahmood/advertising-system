ae3ce012c07a1a4acf3766524966c29176e3faf8
Mahmood Zamani <imahmoodzamani@gmail.com>
2017-07-25 20:05:30 +0430

    add ignore

---
+++ build/0.3670a184d608861c0e46.hot-update.js
@@ -0,0 +1,11 @@
+exports.id = 0;
+exports.modules = {
+
+/***/ "./build/assets.json":
+/***/ (function(module, exports) {
+
+eval("module.exports = {\n\t\"client\": {\n\t\t\"js\": \"http://localhost:3001/static/js/client.js\"\n\t}\n};//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9idWlsZC9hc3NldHMuanNvbi5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2J1aWxkL2Fzc2V0cy5qc29uP2Q2ZjkiXSwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSB7XG5cdFwiY2xpZW50XCI6IHtcblx0XHRcImpzXCI6IFwiaHR0cDovL2xvY2FsaG9zdDozMDAxL3N0YXRpYy9qcy9jbGllbnQuanNcIlxuXHR9XG59O1xuXG5cbi8vLy8vLy8vLy8vLy8vLy8vL1xuLy8gV0VCUEFDSyBGT09URVJcbi8vIC4vYnVpbGQvYXNzZXRzLmpzb25cbi8vIG1vZHVsZSBpZCA9IC4vYnVpbGQvYXNzZXRzLmpzb25cbi8vIG1vZHVsZSBjaHVua3MgPSAwIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ })
+
+};
